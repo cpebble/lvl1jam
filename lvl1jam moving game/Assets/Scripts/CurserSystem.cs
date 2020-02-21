5cf3e8d220144b62ead5c48ea414f79090b06ca3
Rasmus Clausen <vez64ryj@gmail.com>
2020-02-21 20:56:36 +0100

    started work on curser system and grid system

---
+++ lvl1jam moving game/Assets/Scripts/CurserSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurserSystem : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask gridLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit;
+        // hit = Physics2D.OverlapPoint(mousePos,gridLayer,-100f,100f);
+    }
+}
---
+++ lvl1jam moving game/Assets/Scripts/Grid System/PlacementGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementGrid : MonoBehaviour
+{
+    [SerializeField]
+    Cell[] cells;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
