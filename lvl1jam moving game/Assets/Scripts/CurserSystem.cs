7a3713ee69b47a6231d9d91932d47a985a42ee7e
Rasmus Clausen <vez64ryj@gmail.com>
2020-02-22 00:11:18 +0100

    work on cursorsystem

--- lvl1jam moving game/Assets/Scripts/CurserSystem.cs
+++ lvl1jam moving game/Assets/Scripts/CurserSystem.cs
@@ -6,18 +6,58 @@ public class CurserSystem : MonoBehaviour
 {
     [SerializeField]
     LayerMask gridLayer;
-
+    [SerializeField]
+    private PlacementGrid placementGrid;
+    private Transform currentObject;
+    private DragAbleObject currentDragAble;
+    private Vector2 objectOffset;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void Grab(){
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Collider2D col;
+        col = Physics2D.OverlapPoint(mousePos,gridLayer,-100f,100f);
+        if(col != null && col.transform.CompareTag("DragAbleObject"))
+        {
+            
+            currentObject = col.transform.parent;
+            objectOffset = (Vector2)currentObject.position - mousePos;
+            if(currentObject.GetComponent<DragAbleObject>().IsOnGrid()){
+                currentObject.GetComponent<DragAbleObject>().RemoveFromGrid();
+            }
+            currentDragAble = currentObject.GetComponent<DragAbleObject>();
+        }
+    }
+    void Drop(){
+        if(CheckIfDropAble())
+        {
+            currentDragAble.AddToGrid(placementGrid);   
+        }
+        currentObject = null;
+        objectOffset = Vector2.zero;
+        
+    }
+    bool CheckIfDropAble(){
+        return currentObject.GetComponent<DragAbleObject>().CheckIfOnGrid();
+    }
+    void MoveObject(){
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentObject.position = mousePos + objectOffset;
+    }
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit;
-        // hit = Physics2D.OverlapPoint(mousePos,gridLayer,-100f,100f);
+        if(Input.GetMouseButtonDown(0))
+            Grab();
+        if(currentObject != null)
+        {
+            MoveObject();
+        }
+        if(Input.GetMouseButtonUp(0))
+            Drop();
     }
 }
--- lvl1jam moving game/Assets/Scripts/Grid System/DragAbleObject.cs
+++ lvl1jam moving game/Assets/Scripts/Grid System/DragAbleObject.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class DragAbleObject : MonoBehaviour
 {
     [SerializeField]
-    Cell[] cells;
+    Cell[] cells = new Cell[5];
+    [SerializeField]
+    LayerMask gridLayer;
     
+    private PlacementGrid currentGrid;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
+    public Cell GetCell(int index){
+        return cells[0];
+    }
+    public void RemoveFromGrid(){
+        currentGrid.RemoveObject(this);
+    }
+    public void AddToGrid(PlacementGrid grid){
+        grid.Place(this);
+        currentGrid = grid;
+    }
+    public bool IsOnGrid(){
+        return currentGrid != null;
+    }
+    public bool CheckIfOnGrid()
+    {
+        List<Cell> overlapCells = new List<Cell>();
+        foreach (Cell c in cells)
+        {
+            Collider2D col;
+            col = Physics2D.OverlapPoint(c.transform.position, gridLayer, -100f, 100f);
+            if (col != null && col.transform.CompareTag("PlacementGrid"))
+            {
+                if(overlapCells.Contains(c))
+                    return false;
+                overlapCells.Add(col.GetComponent<Cell>());
+            }
+        }
+        //print("overlap"+overlapCells.Count);
+        //print("cells:"+cells.Length);
+        return overlapCells.Count == cells.Length;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
