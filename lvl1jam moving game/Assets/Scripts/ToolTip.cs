6bb1d2c87b3f927b09d7b5aa7089fbe83bfe110a
Christian Påbøl Jacobsen <cpcp800@auroragames.dk>
2020-02-22 00:12:04 +0100

    Added initial tooltip

---
+++ lvl1jam moving game/Assets/Scripts/ToolTip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTip : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject tooltipPrefab;
+
+    private GameObject tooltipGo;
+
+    [TextArea(3, 5)]
+    public string Tip;
+    public string Header;
+    public string Subtext;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Start()
+    {
+        tooltipGo = Instantiate(tooltipPrefab, this.transform);
+        tooltipGo.SetActive(false);
+    }
+
+
+}
