48d807b2acf1c6a5043746934f8e07dae91dc97d
Christian Påbøl Jacobsen <cpcp800@auroragames.dk>
2020-02-22 11:50:33 +0100

    RusmussumsuR

--- lvl1jam moving game/Assets/Scripts/ToolTip.cs
+++ lvl1jam moving game/Assets/Scripts/ToolTip.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ToolTip : MonoBehaviour
@@ -9,6 +10,7 @@ public class ToolTip : MonoBehaviour
     private GameObject tooltipPrefab;
 
     private GameObject tooltipGo;
+    private bool shouldShowTooltip = false;
 
     [TextArea(3, 5)]
     public string Tip;
@@ -17,13 +19,23 @@ public class ToolTip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        tooltipGo.SetActive(shouldShowTooltip);
     }
     void Start()
     {
         tooltipGo = Instantiate(tooltipPrefab, this.transform);
         tooltipGo.SetActive(false);
+        TextMeshProUGUI[] texts = tooltipGo.GetComponentsInChildren<TextMeshProUGUI>();
+        texts[0].text = Tip;
+        texts[1].text = Header;
     }
-
+    public void ShowTooltip()
+    {
+        shouldShowTooltip = true;
+    }
+    public void HideTooltip()
+    {
+        shouldShowTooltip = false;
+    }
 
 }
