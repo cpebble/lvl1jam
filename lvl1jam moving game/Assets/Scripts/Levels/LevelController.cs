46ba8c27f50c5cb1ba2fbeb67a7fc57ca9179abd
Rasmus Clausen <vez64ryj@gmail.com>
2020-02-23 14:22:21 +0100

    trashcan implemented

---
+++ lvl1jam moving game/Assets/Scripts/Levels/LevelController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour, ILevelEventHandler
+{
+    int amountOfTimesPressed = 1;
+    [SerializeField]
+    Animation trashAnimation;
+
+    public bool animationIsFinished;
+    [SerializeField]
+    LevelLoader loader;
+    
+    private bool buttonIsActive = true;
+
+    private bool loadNext = true;
+    void Start()
+    {
+        loader.RegisterEventHandler(this);
+    }
+    public void FinishAnimation(){
+        animationIsFinished = true;
+    }
+    public void ChooseToMove(){
+        if(!buttonIsActive)
+            return;
+        if(!loadNext)
+        {
+            if(trashAnimation == null)
+                trashAnimation = GameObject.Find("Placement grid and trashcan(Clone)").GetComponent<Animation>();
+                
+
+            trashAnimation.Play();
+        }
+        if(loadNext){
+            loader.LoadNextLevel();
+        }
+        loadNext = !loadNext;
+    }
+    
+    public void HandleEvent(LevelLoadState state)
+    {
+        switch (state)
+        {
+            case LevelLoadState.PREUNLOAD:
+                buttonIsActive = false;
+                break;
+            case LevelLoadState.POSTLOAD:
+                buttonIsActive = true;
+                break;
+            
+        }
+    }
+}
